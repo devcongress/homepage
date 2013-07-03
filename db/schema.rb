b5dab2629c783e2e76cef8ec563cb049d0f6087b
yawboakye <backslashr@gmail.com>
2013-07-03 19:51:51 +0000

    add index on email column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130703192942) do
+ActiveRecord::Schema.define(version: 20130703193516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(version: 20130703192942) do
     t.datetime "updated_at"
     t.string   "email"
   end
+
+  add_index "attendees", ["email"], name: "index_attendees_on_email", unique: true, using: :btree
 
 end
