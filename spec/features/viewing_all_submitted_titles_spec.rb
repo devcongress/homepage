e21ff1ad75dc581670715c31311b83cfa84dd9d1
yawboakye <backslashr@gmail.com>
2013-07-14 16:01:17 +0000

    redirect to all titles if title is not found

--- spec/features/viewing_all_submitted_titles_spec.rb
+++ spec/features/viewing_all_submitted_titles_spec.rb
@@ -4,7 +4,6 @@ feature 'Viewing titles' do
 	
 	scenario 'Listing all submitted titles' do
 		title = FactoryGirl.create :title
-		puts Title.all.count
 		visit titles_url
 		click_link 'Database Normalization'
 		expect(page.current_url).to eql(title_url(title))
