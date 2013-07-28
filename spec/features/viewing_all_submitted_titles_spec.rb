59bf4d3735972f50169ca2c5f40311ab3e5144c4
yawboakye <backslashr@gmail.com>
2013-07-28 22:47:53 +0000

    providing more information about submitted titles

--- spec/features/viewing_all_submitted_titles_spec.rb
+++ spec/features/viewing_all_submitted_titles_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 feature 'Viewing titles' do
-	
-	scenario 'Listing all submitted titles' do
-		title = FactoryGirl.create :title, title: 'Wading Through JavaScript MVC Jungle'
+	before do
+		@title = FactoryGirl.create :title, title: 'Wading Through JavaScript MVC Jungle'
 		visit titles_url
+	end
+
+	scenario 'Listing all submitted titles' do
+		expect(page).to have_content 'Wading Through JavaScript MVC Jungle'
+		expect(page.current_url).to eql(titles_url)
+	end
+
+	scenario 'Viewing a submitted title' do
 		click_link 'Wading Through JavaScript MVC Jungle'
-		expect(page.current_url).to eql(title_url(title))
+
+		expect(page.current_url).to eql title_url @title
+		expect(page).to have_content 'A talk on Wading Through JavaScript MVC Jungle submitted/suggested by Joey Barton'
 	end
 
 end
