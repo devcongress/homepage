e93537a49e5bbe252d10b868363c0eb12e08f160
yawboakye <backslashr@gmail.com>
2013-08-02 13:46:07 +0000

    SMPT (using Mailchimp's Mandrill) settings

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings = {
+						 user_name: 'app16661060@heroku.com',
+							password: 'drkcovtB5K5aDJ6GSdkAwQ',
+								domain: 'peaceful-woodland-7453.herokuapp.com',
+							 address: 'smtp.mandrillapp.com',
+									port: 587,
+				authentication: :plain,
+	enable_starttls_auto: true
+}
+
 # Initialize the Rails application.
 Devconf::Application.initialize!
---
+++ config/initializers/smtp_config.rb
@@ -0,0 +1,6 @@
+Devconf::Application.configure do
+	config.action_mailer.delivery_method 		= :smtp
+	config.action_mailer.perform_deliveries = true
+
+	
+end
