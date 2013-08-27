d98cc112fb54d5859ead9c8edc118ab03ab744f6
--global <backslashr@gmail.com>
2013-08-27 15:12:18 +0000

    thank you email finally ok to go, or I think so

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,6 +1,6 @@
 class Notifier < ActionMailer::Base
   default from: 		'devcongress@gmail.com',
-  				reply_to: 'devcongress@gmail.com',
+  				reply_to: 'devcongress@gmail.com'
 
   def comments_updated(comment)
   	@comment = comment
@@ -9,8 +9,10 @@ class Notifier < ActionMailer::Base
   end
 
 	def thank_you
-		mail(to: Proc.new { ThankYouList.pluck :email }, subject: 'Thank you for making DevCongress successful. You\'re awesome!')
+		mail(
+					 to: Proc.new { ThankYouList.pluck :email },
+			subject: 'Thank you for making DevCongress successful. You\'re awesome!'
+		)
 	end
-
 
 end
