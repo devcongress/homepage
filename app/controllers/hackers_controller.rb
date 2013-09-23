de4f0bed3d7d5b5c5619b06d1f6700acc5b2d391
--global <yawboakye10@gmail.com>
2013-09-23 03:46:54 +0000

    Set up email notification for hacker registration.
    One of the tests fails because, well, I guess redirect_to
    makes a lot of noise without explaining anything to me

--- app/controllers/hackers_controller.rb
+++ app/controllers/hackers_controller.rb
@@ -2,6 +2,7 @@ class HackersController < ApplicationController
   def create
   	hacker = Hacker.new hacker_params
   	if hacker.save
+  		Notifier.hacker_registered(hacker).deliver
   		redirect_to mission_path,
   			notice: 'We have sent you an email. Please follow instructions in the mail \
   							 to complete your registration. Thank you and see you there!'
