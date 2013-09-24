cf9cdad248323725b8778458dac764a56c439814
Claude Ayitey <clauditey@gmail.com>
2013-09-24 22:36:13 +0000

    I disabled the following routes, /agenda, /submit_title, /speakers, /requirements, /say_thanks. These routes are no longer active and are not related to the current project. For those routes that may be related, for example, /requirements, its contents do not match that of the DevCongress Hackathon. I also re-routed the application root to /hack. In the mean time, while I'm not done designing the index.html.erb file, I would want to direct all attention to the registration page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Devconf::Application.routes.draw do
-  root to: 'pages#index'
+  root to: 'pages#hack'
 
   resources :attendees
 
@@ -11,13 +11,13 @@ Devconf::Application.routes.draw do
 
 
   match '/register',      to: 'attendees#new',      via: :get
-  match '/agenda',        to: 'pages#agenda',       via: :get
-  match '/submit_title',  to: 'titles#index',       via: :get
-  match '/speakers',      to: 'pages#speakers',     via: :get
+  # match '/agenda',        to: 'pages#agenda',       via: :get
+  # match '/submit_title',  to: 'titles#index',       via: :get
+  # match '/speakers',      to: 'pages#speakers',     via: :get
   match '/about',         to: 'pages#about',        via: :get
   match '/reg',           to: 'pages#reg',          via: :get
-  match '/requirements',  to: 'pages#requirements', via: :get
-  match '/say_thanks',    to: 'pages#say_thanks',   via: :get
+  # match '/requirements',  to: 'pages#requirements', via: :get
+  # match '/say_thanks',    to: 'pages#say_thanks',   via: :get
   match '/hack',          to: 'pages#hack',         via: :get
   match '/mission',       to: 'pages#mission',      via: :get
 end
