125a45fb5f1e9c191a1b77ed1cbd7b6b2751f1f1
Claude Ayitey <clauditey@gmail.com>
2013-10-03 19:52:59 +0000

    added routes for the new website, each project will have its own controllers now

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,17 @@ Devconf::Application.routes.draw do
   post 'hackers', to: 'hackers#create'
 
 
-  match '/register',      to: 'attendees#new',      via: :get
-  # match '/agenda',        to: 'pages#agenda',       via: :get
-  # match '/submit_title',  to: 'titles#index',       via: :get
-  # match '/speakers',      to: 'pages#speakers',     via: :get
-  match '/about',         to: 'pages#about',        via: :get
-  match '/reg',           to: 'pages#reg',          via: :get
-  # match '/requirements',  to: 'pages#requirements', via: :get
-  # match '/say_thanks',    to: 'pages#say_thanks',   via: :get
-  match '/hack',          to: 'pages#hack',         via: :get
-  match '/mission',       to: 'pages#mission',      via: :get
+  # match '/register',      to: 'attendees#new',          via: :get
+  # match '/agenda',        to: 'pages#agenda',           via: :get
+  # match '/submit_title',  to: 'titles#index',           via: :get
+  # match '/speakers',      to: 'pages#speakers',         via: :get
+  match '/about',         to: 'pages#about',            via: :get
+  # match '/reg',           to: 'pages#reg',              via: :get
+  # match '/requirements',  to: 'pages#requirements',     via: :get
+  match '/say_thanks',    to: 'pages#say_thanks',       via: :get
+  match '/hack',          to: 'pages#hack',             via: :get
+  # match '/mission',       to: 'pages#mission',          via: :get
+  # match '/codecamp'       to: 'codecamp#index',         via: :get
+  # match '/hack'           to: 'hackathon#index',        via: :get
+  # match '/devcongress',   to: 'devcongress#index',      via: :get
 end
