b3217c26961fea46a62a44a5d0f3831b4a7041e5
yawboakye <wheresyaw@gmail.com>
2013-11-04 10:14:11 +0000

    changed indentation from tabs to spaces

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :title
 
   validates :commenter_name, presence: true
-  validates :comment_body,	  presence: true
-  
+  validates :comment_body,   presence: true
+
 end
