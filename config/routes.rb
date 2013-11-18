ead2b4615bd8f678b3c20f0d9f4e01155e7a169e
Claude Ayitey <clauditey@gmail.com>
2013-11-18 18:50:47 +0000

    cleaned up unused routes in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,7 @@ Devconf::Application.routes.draw do
 
   post 'hackers', to: 'hackers#create'
 
-
   # match '/register',      to: 'attendees#new',          via: :get
-  # match '/agenda',        to: 'pages#agenda',           via: :get
-  # match '/submit_title',  to: 'titles#index',           via: :get
-  # match '/speakers',      to: 'pages#speakers',         via: :get
-  # match '/reg',           to: 'pages#reg',              via: :get
   # match '/requirements',  to: 'pages#requirements',     via: :get
   # match '/mission',       to: 'pages#mission',          via: :get
   # match '/hack'           to: 'hackathon#index',        via: :get
