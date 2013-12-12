e103d741e6a6b61048f5b51421acdde9d54a9f9d
Claude Ayitey <clauditey@gmail.com>
2013-12-12 20:37:01 +0000

    changes to schema **

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131118155127) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "attendees", force: true do |t|
     t.string   "name"
     t.string   "website"
