347ccb2ad587e5d47c1b0928226828988dfb3502
yawboakye <wheresyaw@gmail.com>
2014-01-16 13:17:57 +0000

    reduce whitespace to 2 spaces
    @clauditey should set his text editor to 2 spaces (instead of tab) for
    indentation on this project. While the code appears beautifully laid out
    in the text editor, Github has issues rendering tab-indented code. It
    transforms it to 8 instead of 4

--- app/mailers/title_notifier.rb
+++ app/mailers/title_notifier.rb
@@ -1,11 +1,11 @@
 class TitleNotifier < ActionMailer::Base
-  default from: 		'devcongress@gmail.com',
-  				reply_to: 'devcongress@gmail.com'
+  default from:     'devcongress@gmail.com',
+          reply_to: 'devcongress@gmail.com'
 
   def submission_successful title
-  	@title = title
-  	email_with_name = "#{@title.submitter_name} <#{@title.submitter_email}>"
-  	mail to: email_with_name, subject: '[DevCongress 24.08.13] Confirmation of Receipt'
+    @title = title
+    email_with_name = "#{@title.submitter_name} <#{@title.submitter_email}>"
+    mail to: email_with_name, subject: '[DevCongress 24.08.13] Confirmation of Receipt'
   end
 
 end
