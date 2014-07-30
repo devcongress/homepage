c1b0b86e54fdc744ba32b505b9e2716900586502
Claude Ayitey <clauditey@gmail.com>
2014-07-30 20:29:42 +0000

    added different routes for news, exchange, and forge.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Devconf::Application.routes.draw do
     resources :comments
   end
 
-  get '/exchange'   => 'pages#exchange'
+  # get '/exchange'   => 'pages#exchange'
   get '/codecamp'   => 'pages#codecamp'
   get '/thanks'		=> 'pages#thankyou'
+  get '/news'		=> redirect('http://news.devcongress.com')
+  get '/forge'		=> redirect('http://forge.devcongress.com')
+  get '/exchange'	=> redirect('http://exchange.devcongress.com')
 
 end
